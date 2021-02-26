131fbb46f8356b41d3316295ae6e38bb657f2b2b
vidya1900 <vidyashrikalshetti@gmail.com>
2021-02-26 10:16:11 +0530

    first Commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import NewsCard from './newsComp';
+import React from "react";
+import Customers from "./components/customers";
+
+function App() {
+  return (
+    <div >
+    <Customers/>
+    </div>
+  );
+}
+
+export default App;
